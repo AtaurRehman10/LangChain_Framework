56050861f2e9f3bbdda9f77e66d45c2c98d800dc
AtaurRehman <ataulrehmangee994@gmail.com>
2026-07-08 06:32:36 +0500

    update

--- output/simple_LLMoutput.py
+++ output/simple_LLMoutput.py
@@ -6,8 +6,6 @@
 # pydantic 
 # JSON Schema
 
-from urllib import response
-
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
